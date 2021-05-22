6a9543ab43f334cbc1b00e1fcfa1da5d9ee3f8d2
eswar2001 <teswar2001@gmail.com>
2021-05-22 17:25:18 +0530

    final try

--- index.js
+++ index.js
@@ -42,66 +42,66 @@ bot.on('message', (msg) => {
         var today = dd + '-' + mm + '-' + yyyy;
         var query = 'pincode=' + pincode + '&date=' + today.toString();
         axios.get(baseurl + encodeURI(query), {
-            headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36' }
-        },
-            (res) => {
-                res.data
-            }).then((response) => {
-                if (response.data.centers.length == 0) {
-                    bot.sendMessage(chatId, "No Centers Available");
-                } else {
-                    // console.log(baseurl + encodeURI(query));
-                    var centerNames = "";
-                    var details = "";
-                    for (i = 0; i < Object.keys(response.data.centers).length; i++) {
-                        var flag = false;
-                        // console.log('length');
-                        // console.log(Object.keys(response.data.centers[i].sessions).length);
-                        for (var j = 0; j < Object.keys(response.data.centers[i].sessions).length; j++) {
-                            // console.log(response.data.centers[i].sessions[j]["available_capacity"]);
-                            if (response.data.centers[i].sessions[j]["available_capacity"] > 0) {
-                                flag = true;
-                                centerNames += `\r\n\r\n<b><strong>${response.data.centers[i]["name"]}</strong></b>`;
-                                details += `\r\nMinAge Limit: ${response.data.centers[i].sessions[j].min_age_limit} \r\n${response.data.centers[i]["name"]}` + "\r\n BLock Name:" + `${response.data.centers[i]["block_name"]}` + '\r\nAvailable capacity:' +
-                                    response.data.centers[i].sessions[j]["available_capacity"].toString();
-                                details += `\r\nAvailable on: ${today.toString()}`;
-                                details += '\r\n slots \r\n';
-                                for (var k = 0; k < Object.keys(response.data.centers[i].sessions[j].slots).length; k++) {
-                                    details += `${response.data.centers[i].sessions[j].slots[k]}` + '\r\n';
-                                }
-                                details += `available_capacity_dose1: ${response.data.centers[i].sessions[j].available_capacity_dose1}\r\navailable_capacity_dose2: ${response.data.centers[i].sessions[j].available_capacity_dose2}`;
-                                if (response.data.centers[i].sessions[j]["vaccine"] != '') {
-                                    details += `\r\nVaccine type: ${response.data.centers[i].sessions[j]["vaccine"]}\r\n`;
-                                }
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+            }
+        }, (res) => {
+            res.data
+        }).then((response) => {
+            if (response.data.centers.length == 0) {
+                bot.sendMessage(chatId, "No Centers Available");
+            } else {
+                // console.log(baseurl + encodeURI(query));
+                var centerNames = "";
+                var details = "";
+                for (i = 0; i < Object.keys(response.data.centers).length; i++) {
+                    var flag = false;
+                    // console.log('length');
+                    // console.log(Object.keys(response.data.centers[i].sessions).length);
+                    for (var j = 0; j < Object.keys(response.data.centers[i].sessions).length; j++) {
+                        // console.log(response.data.centers[i].sessions[j]["available_capacity"]);
+                        if (response.data.centers[i].sessions[j]["available_capacity"] > 0) {
+                            flag = true;
+                            centerNames += `\r\n\r\n<b><strong>${response.data.centers[i]["name"]}</strong></b>`;
+                            details += `\r\nMinAge Limit: ${response.data.centers[i].sessions[j].min_age_limit} \r\n${response.data.centers[i]["name"]}` + "\r\n BLock Name:" + `${response.data.centers[i]["block_name"]}` + '\r\nAvailable capacity:' +
+                                response.data.centers[i].sessions[j]["available_capacity"].toString();
+                            details += `\r\nAvailable on: ${today.toString()}`;
+                            details += '\r\n slots \r\n';
+                            for (var k = 0; k < Object.keys(response.data.centers[i].sessions[j].slots).length; k++) {
+                                details += `${response.data.centers[i].sessions[j].slots[k]}` + '\r\n';
                             }
-                        }
-                        if (flag && response.data.centers[i]["fee_type"] == "Paid") {
-                            for (j = 0; j < Object.keys(response.data.centers[i]["vaccine_fees"]).length; j++) {
-                                details += `${response.data.centers[i].vaccine_fees[j].vaccine} Price: ${response.data.centers[i]["vaccine_fees"][j]["fee"]}\r\n\r\n`;
+                            details += `available_capacity_dose1: ${response.data.centers[i].sessions[j].available_capacity_dose1}\r\navailable_capacity_dose2: ${response.data.centers[i].sessions[j].available_capacity_dose2}`;
+                            if (response.data.centers[i].sessions[j]["vaccine"] != '') {
+                                details += `\r\nVaccine type: ${response.data.centers[i].sessions[j]["vaccine"]}\r\n`;
                             }
-                            flag = false;
                         }
                     }
-                    // console.log(details);
-                    if (details == '') {
-                        details += centerNames + "\r\n No Vaccinations available";
-                        bot.sendMessage(chatId, details);
-                    } else {
-                        if (centerNames != '') {
-                            var reply = sendMail({ email, msg: details })
-                            reply.then((data) => {
-                                bot.sendMessage(chatId, `Please visit ${data} \r\n ${details}`);
-                            })
-                        } else {
-                            bot.sendMessage(chatId, "Currently there are no centers available");
+                    if (flag && response.data.centers[i]["fee_type"] == "Paid") {
+                        for (j = 0; j < Object.keys(response.data.centers[i]["vaccine_fees"]).length; j++) {
+                            details += `${response.data.centers[i].vaccine_fees[j].vaccine} Price: ${response.data.centers[i]["vaccine_fees"][j]["fee"]}\r\n\r\n`;
                         }
+                        flag = false;
                     }
                 }
-            }).catch((error) => {
-                console.log(error);
-                bot.sendMessage(chatId, msg.text + 'Message format "pincode EmailID"');
-            })
-
+                // console.log(details);
+                if (details == '') {
+                    details += centerNames + "\r\n No Vaccinations available";
+                    bot.sendMessage(chatId, details);
+                } else {
+                    if (centerNames != '') {
+                        var reply = sendMail({ email, msg: details })
+                        reply.then((data) => {
+                            bot.sendMessage(chatId, `Please visit ${data} \r\n ${details}`);
+                        })
+                    } else {
+                        bot.sendMessage(chatId, "Currently there are no centers available");
+                    }
+                }
+            }
+        }).catch((error) => {
+            console.log(error);
+            bot.sendMessage(chatId, msg.text + " " + error.response.status + ' Message format "pincode EmailID"');
+        })
     }
 });
 
